11fab1e301a7568e8609931ef070e46dda1ab714
Carlos Garcia <cj.garcias1@uniandes.edu.co>
2026-03-04 22:10:53 -0500

    Update Timescale migration SQL for readability

--- receiver/migrations/to_timescale.py
+++ receiver/migrations/to_timescale.py
@@ -1,8 +1,5 @@
 from django.db import migrations
 
-# Migraciones para configuración de la base de datos Postgres con la extensión de Timescale.
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,18 +7,17 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # Crea la hipertabla de timescale con chunks de 3 días.
+        # 3 days chunk interval
         migrations.RunSQL(
-            "SELECT create_hypertable('\"receiver_data\"', 'time', chunk_time_interval=>259200000000);"
+            "SELECT create_hypertable('\"receiver_data\"', 'time', chunk_time_interval => INTERVAL '3 days');"
         ),
-        # Configura la compresión para estaciones y variables. Son llaves foráneas de la tabla principal.
         migrations.RunSQL(
             "ALTER TABLE \"receiver_data\" \
                 SET (timescaledb.compress, \
                 timescaledb.compress_segmentby = 'station_id, measurement_id, base_time');"
         ),
-        # Comprime los datos cada 7 días.
+        # Compress after 7 days
         migrations.RunSQL(
-            "SELECT add_compression_policy('\"receiver_data\"', 604800000000);"
+            "SELECT add_compression_policy('\"receiver_data\"', INTERVAL '7 days');"
         ),
     ]
